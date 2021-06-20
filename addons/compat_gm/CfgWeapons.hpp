bb864e6cdd84f16793ab318e5e9c2179e4409aab
nomisum <nomisum@gmail.com>
2021-06-20 22:02:27 +0200

    add overheating to machine guns, overpressure to launchers

--- addons/compat_gm/CfgWeapons.hpp
+++ addons/compat_gm/CfgWeapons.hpp
@@ -1,4 +1,34 @@
 class CfgWeapons {
+
+    // MACHINE GUNS
+    class gm_pk_base;
+    class gm_pkm_base: gm_pk_base {
+        ace_overheating_mrbs = 3000;
+        ace_overheating_slowdownFactor = 1;
+        ace_overheating_allowSwapBarrel = 1;
+        ace_overheating_dispersion = 0.25;
+    };
+    class gm_machineGun_base;
+    class gm_mg3_base: gm_machineGun_base {
+        ace_overheating_mrbs = 3000;
+        ace_overheating_slowdownFactor = 1;
+        ace_overheating_allowSwapBarrel = 1;
+        ace_overheating_dispersion = 0.25;
+    };
+    class gm_mg3_veh_base: gm_mg3_base {
+        ace_overheating_allowSwapBarrel = 0;
+    };
+    class gm_launcher_base;
+    class gm_carlgustaf_m2_base: gm_launcher_base {
+        ace_reloadlaunchers_enabled = 1;
+        ace_overpressure_angle = 60;
+        ace_overpressure_damage = 0.7;
+        ace_overpressure_priority = 1;
+        ace_overpressure_range = 10;
+    };
+
+
+    // HELMETS
     #define HEARING_PROTECTION_OPEN EGVAR(hearing,protection) = 0; EGVAR(hearing,lowerVolume) = 0;
     #define HEARING_PROTECTION_VICCREW EGVAR(hearing,protection) = 0.85; EGVAR(hearing,lowerVolume) = 0.6;
     #define HEARING_PROTECTION_EARMUFF EGVAR(hearing,protection) = 0.75; EGVAR(hearing,lowerVolume) = 0.5;
